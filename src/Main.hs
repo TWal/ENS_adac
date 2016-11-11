45afa29b72ded454dcfd6b9627b323f07e523304
Théophile Wallez <theophile@wallez.net>
2016-11-11 10:27:27 +0100

    Move the `map toLower` in Lexer

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 import Parser
-import Data.Char (toLower)
 
 main :: IO ()
 main = do
     s <- getContents
-    print $ parseFichier (map toLower s) "<stdin>"
+    print $ parseFichier s "<stdin>"
