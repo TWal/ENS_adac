03aacee0acc643241e88f339ac9be0f99987d398
lucas8 <luc.linux@mailoo.org>
2016-11-21 23:55:54 +0100

    Implement type_access

--- src/Typer.hs
+++ src/Typer.hs
@@ -12,24 +12,19 @@ import qualified Control.Monad.Trans.State.Strict as S
 data Pile a = a :^: Pile a | Bottom a
 
 -------------- General types definitions --------------------------------------
-data TypeClass = TypeClass 
-    { typify   :: Typed -> Either String ()
-    , typename :: String
-    }
-
 data Typed = TInteger
            | TCharacter
            | TBoolean
            | TRecord String
            | TAccess String
-           | Typenull
+           | TypeNull
 instance Eq Typed where
     TInteger   == TInteger   = True
     TCharacter == TCharacter = True
     TBoolean   == TBoolean   = True
-    Typenull   == Typenull   = True
     TRecord n1 == TRecord n2 = n1 == n2
     TAccess n1 == TAccess n2 = n1 == n2
+    TypeNull   == TypeNull   = True
     _          == _          = False
 data Functionnal = TFunction TParams Typed | TProcedure TParams
 data TParams = TParams [(String,CType)]
@@ -38,7 +33,7 @@ is_defined :: Recorded -> Bool
 is_defined RNotDefined = False
 is_defined _           = True
 
-data CType = RValue Typed | RLValue Typed | LValue TypeClass
+data CType = RValue Typed | RLValue Typed | LValue Typed
 data Context = Context
     { variables :: Map String CType
     , functions :: Map String Functionnal
@@ -52,8 +47,6 @@ instance Show TParams where
                                  ++ ", " ++ show tps
     show (TParams [(nm,t)])       = nm ++ " : " ++ show t
     show (TParams [])             = ""
-instance Show TypeClass where
-    show t = typename t
 instance Show CType where
     show (RValue t)  = "out "    ++ show t
     show (LValue t)  = "in "     ++ show t
@@ -64,7 +57,7 @@ instance Show Typed where
     show TBoolean    = "Bool"
     show (TRecord n) = "Record " ++ n
     show (TAccess n) = "Access " ++ n
-    show Typenull    = "Typenull"
+    show TypeNull    = "Null"
 instance Show Functionnal where
     show (TFunction tp t) = "(" ++ show tp ++ " -> " ++ show t ++ ")"
     show (TProcedure tp)  = "(" ++ show tp ++ ")"
@@ -238,23 +231,6 @@ data TInstr =
   | TIFor String Bool TPExpr TPExpr (NonEmptyList TInstr)
   | TIWhile TPExpr (NonEmptyList TInstr)
 
--------------- Internals types ------------------------------------------------
-null :: TypeClass
-null = TypeClass tpf "null"
- where tpf :: Typed -> Either String ()
-       tpf (TAccess _) = Right ()
-       tpf t           = fail $ show t ++ " has no null value"
-
-num :: TypeClass
-num = TypeClass tpf "numeric"
- where tpf TInteger = Right ()
-       tpf t        = fail $ "can't convert numeric to " ++ show t
-
-get_class :: Typed -> TypeClass
-get_class t = TypeClass (tpf t) $ show t
- where tpf t1 t2 = if t1 == t2 then Right ()
-        else fail $ "can't convert " ++ show t1 ++ " to " ++ show t2
-
 -------------- Getting the typing done ----------------------------------------
 showPos :: AlexPosn -> String
 showPos = show
@@ -312,7 +288,7 @@ type_type (Access (Ident nm,p))   = case nm of
 
 
 type_decls :: [Ann Decl AlexPosn] -> Env TDecls
-type_decls _ = undefined
+type_decls dcls = CM.foldM (flip td) empty_tdecls dcls
  where td :: Ann Decl AlexPosn -> TDecls -> Env TDecls
        td (DType (Ident s, p1),p2) tds = addt_if tds p1 s RNotDefined
        td (DAccess (Ident s1, p1) (Ident s2, p2), p3) tds = case s2 of
@@ -329,6 +305,7 @@ type_decls _ = undefined
        td (DAssign ids (tp, ptp) (Just e), pa) tds = do
            t  <- type_type tp
            ne <- type_expr e -- TODO check if ne is lvalue with compatible type
+           undefined
            CM.foldM (\mp -> \(Ident i,p) -> addv_if mp p i
                $ (RLValue t,Just $ Left ne)) tds
                $ non_empty_to_list ids
@@ -379,13 +356,14 @@ type_decls _ = undefined
                               else return ()
            t <- type_type rtp
            push_env nm
-           addVar nm pf $ LValue $ get_class t -- Adding the function
+           addVar nm pf $ LValue t -- Adding the function
            type_decls dcls
            li <- CM.mapM (type_instr_typed t) $ non_empty_to_list instrs
            pop_env
            addv_if tds pf nm
-               (LValue $ get_class t, Just $ Right $ list_to_non_empty li)
+               (LValue t, Just $ Right $ list_to_non_empty li)
 
+       empty_tdecls = TDecls M.empty M.empty M.empty
        drop3 :: (a,b,c) -> (a,b)
        drop3 (x,y,z) = (x,y)
        addt_if :: TDecls -> AlexPosn -> String -> Recorded -> Env TDecls
@@ -438,9 +416,9 @@ type_params (Params prs,pos) = do
        tpr ((Ident nm,pnm), md, (tp, ptp)) = do
            t <- type_type tp
            let ctp = case md of
-                      Just (In,_)    -> LValue  $ get_class t
+                      Just (In,_)    -> LValue  t
                       Just (InOut,_) -> RLValue $ t
-                      Nothing        -> LValue  $ get_class t
+                      Nothing        -> LValue  t
            return (nm, ctp, pnm)
        fst3 (x,y,z) = x
        mzip :: a -> b -> [c] -> [(c,a,b)]
@@ -458,8 +436,36 @@ type_expr _ = undefined
 
 
 
-type_access :: Acces AlexPosn -> Env TAccess
-type_access _ = undefined
+type_access :: Acces AlexPosn -> Env (TAccess,CType)
+type_access (AccesIdent (Ident s,p)) = do
+    v <- getVar s 
+    case v of
+     Nothing          -> lerror p $ s ++ " is not defined"
+     Just t@(LValue (TAccess s))  -> return (AccessFull s, t)
+     Just t@(RValue (TAccess s))  -> return (AccessFull s, t)
+     Just t@(RLValue (TAccess s)) -> return (AccessFull s, t)
+     Just _                       -> lerror p $ s ++ " is not an access"
+type_access (AccesDot ie@(_,pe) (Ident f, pf)) = do
+    te@(_,tpe) <- type_expr ie
+    rtp <- case tpe of
+     LValue (TAccess s)  -> get_sub s pe f pf >>= \x -> return $ LValue  x
+     LValue (TRecord s)  -> get_sub s pe f pf >>= \x -> return $ LValue  x
+     RLValue (TAccess s) -> get_sub s pe f pf >>= \x -> return $ RLValue x
+     RLValue (TRecord s) -> get_sub s pe f pf >>= \x -> return $ RLValue x
+     RValue (TAccess s)  -> get_sub s pe f pf >>= \x -> return $ RValue  x
+     RValue (TRecord s)  -> get_sub s pe f pf >>= \x -> return $ RValue  x
+     _                   -> lerror pe $ "expression does not evaluate to a record"
+    return (AccessPart te f, rtp)
+ where get_sub :: String -> AlexPosn -> String -> AlexPosn -> Env Typed
+       get_sub s ps f pf = do
+        t <- getTpe s
+        case t of
+         Nothing -> lerror pe $ "expression has unvalid " ++ s ++ " type"
+         Just (Record mp) -> if not $ M.member f mp
+            then lerror pf $ "record " ++ s ++ " has no member " ++ f
+            else return $ mp ! f
+         Just (RAccess mp) -> lerror pf $ "access have no subtypes"
+         Just RNotDefined  -> lerror pe $ s ++ " is declared but not defined"
 
 
 
