0139fdee5556b0d44d38d7fd589fd119fe69fd74
lucas8 <luc.linux@mailoo.org>
2016-12-01 19:00:01 +0100

    Add examples for #4

---
+++ tests/typing/bad/testfile-record-4.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Test is
+   type R is record all: integer; end record;
+begin
+   New_Line;
+end;
+
---
+++ tests/typing/good/testfile-access.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Test is
+   x : access character;
+begin
+   Put(x.all);
+end;
